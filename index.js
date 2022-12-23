a36c7eeca126e527893d91c808b29981e836dd0b
lamnaourm <mohammed.lamnaour@gmail.com>
2022-12-23 09:40:25 +0100

    Correction Atelier 03

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const books = require("./moduleBook");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/catalogue', (req, res) => {
+    res.status(207).json(books.livres);
+})
+
+app.get('/livre/:id', (req, res) => {
+    const id = req.params.id;
+    const livre = books.livres.filter(item => item.id === parseInt(id));
+
+    if(livre.length === 0)
+        res.sendStatus(404);
+    else 
+        res.status(207).json(livre[0]);
+})
+
+
+app.post('/livre', (req, res) => {
+    const livre = req.body;
+    books.AddLivre(livre);
+    res.end();
+})
+
+app.put('/livre', (req, res) => {
+    const livre = req.body;
+    books.livres = books.livres.map(item => item.id === livre.id ? livre:item)
+    res.sendStatus(202);
+})
+
+app.delete('/livre/:id', (req, res) => {
+    const id = req.params.id;
+    const len = livres.length;
+    books.livres = books.livres.filter(item => item.id != id)
+    if(len === livres.length)
+        res.sendStatus(504);
+    else 
+        res.sendStatus(210);
+})
+
+app.listen(3000, () => {
+  console.log("Serveur lance au port 3000");
+});
